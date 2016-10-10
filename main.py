1442c028a7c6b70d857bdb7c868aeb0696be3195
David House <davidhousedev@gmail.com>
2016-10-10 18:59:42 -0400

    STYLE: Remove unnecessary line break

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import jinja2
 import webapp2
 
 TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), 'templates')
-
 JINJA_ENV = jinja2.Environment(loader=jinja2.FileSystemLoader(TEMPLATE_DIR),
                                autoescape=False)
 
