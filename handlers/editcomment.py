2c3930f391372bc6f575cc667c05728e47c28878
David House <davidhousedev@gmail.com>
2016-10-18 12:50:20 -0400

    feat: Finalize comment editing backend logic

--- handlers/editcomment.py
+++ handlers/editcomment.py
@@ -7,6 +7,7 @@ import models.post as db_post
 class EditComment(handler.Handler):
     """ Handler for editing comments """
     def get(self, author, post_id, comment_id):
+        """ Displays comment editing form """
         post = db_post.Post.get_post(author, post_id)
         comment = db_comment.Comment.get_by_id(author,
                                                post_id,
@@ -16,3 +17,16 @@ class EditComment(handler.Handler):
         self.render("editcomment.html",
                     post=post,
                     comment=comment)
+
+    def post(self, author, post_id, comment_id):
+        """ Writes changes to comment to database """
+        comment = db_comment.Comment.get_by_id(author,
+                                               post_id,
+                                               comment_id)
+        if self.user.username == comment.author:  # did user write comment?
+            content = self.request.get("content")
+            db_comment.Comment.edit(author,
+                                    post_id,
+                                    comment_id,
+                                    content)  # edit comment in database
+        self.redirect("/post" "/%s/%s" % (author, post_id))
